11cfce1c28c5f775bceafc8dce8f33e05c53116b
Uladzislau Papko <vpopko3@gmail.com>
2022-04-19 15:26:29 +0300

    init

---
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package net.javaguides.springboot.controller;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
