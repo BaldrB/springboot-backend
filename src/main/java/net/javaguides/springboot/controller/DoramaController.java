b86c81ad1d4c7940ce95429f227f000307ea31fb
Vladislav Popko <vpopko3@gmail.com>
2022-07-15 13:08:46 +0000

    dorama controller

--- src/main/java/net/javaguides/springboot/controller/DoramaController.java
+++ src/main/java/net/javaguides/springboot/controller/DoramaController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import net.javaguides.springboot.message.ResponseMessage;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -25,26 +26,32 @@ public class DoramaController {
     private TagdoramaRepository tagdoramaRepository;
 
     @GetMapping
-    public List<Dorama> getAllSklad(){
+    public List<Dorama> getAllSklad() {
         return doramaRepository.findAll();
     }
 
     // build create employee REAST API
     @PostMapping
-    public Dorama createDorama(@RequestBody Dorama dorama){
+    public ResponseEntity<ResponseMessage> createDorama(@RequestBody Dorama dorama) {
+        String message = "";
+        try {
+            dorama.setDoramaImg("http://code.kupava.by:8080/files/" + dorama.getDoramaImg());
 
-        dorama.setDoramaImg("http://code.kupava.by:8080/files/" + dorama.getDoramaImg());
-
-        for(Tagdorama tag : tagdoramaRepository.findAll()) {
-            dorama.addDoramaTag(tag);
+            for (Tagdorama tag : tagdoramaRepository.findAll()) {
+                dorama.addDoramaTag(tag);
+            }
+            doramaRepository.save(dorama);
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Could not new dorama!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
         }
-        return doramaRepository.save(dorama);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<HttpStatus> deleteDorama(@PathVariable long id){
+    public ResponseEntity<HttpStatus> deleteDorama(@PathVariable long id) {
         Dorama dorama = doramaRepository.findById(id)
-        .orElseThrow(() -> new ResourceNotFoundException("dorma not exist with id" + id));
+                .orElseThrow(() -> new ResourceNotFoundException("dorma not exist with id" + id));
         doramaRepository.delete(dorama);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -53,35 +60,39 @@ public class DoramaController {
     // // build get emloyee by id REST API
     // @GetMapping("{id}")
     // public ResponseEntity<Sklad> getSkladById(@PathVariable long id){
-    //     Sklad sklad = skladRepository.findById(id)
-    //             .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
-    //     return ResponseEntity.ok(sklad);
+    // Sklad sklad = skladRepository.findById(id)
+    // .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with
+    // id:" + id));
+    // return ResponseEntity.ok(sklad);
     // }
 
     // //bulid update employee REST API
     // @PutMapping("{id}")
-    // public ResponseEntity<Sklad> updateSklad(@PathVariable long id, @RequestBody Sklad skladDetails){
-    //     Sklad updateSklad = skladRepository.findById(id)
-    //             .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+    // public ResponseEntity<Sklad> updateSklad(@PathVariable long id, @RequestBody
+    // Sklad skladDetails){
+    // Sklad updateSklad = skladRepository.findById(id)
+    // .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with
+    // id:" + id));
 
-    //     updateSklad.setInvet(skladDetails.getInvet());
-    //     updateSklad.setSystemName(skladDetails.getSystemName());
-    //     updateSklad.setComment(skladDetails.getComment());
-    //     updateSklad.setHardware(skladDetails.getHardware());
+    // updateSklad.setInvet(skladDetails.getInvet());
+    // updateSklad.setSystemName(skladDetails.getSystemName());
+    // updateSklad.setComment(skladDetails.getComment());
+    // updateSklad.setHardware(skladDetails.getHardware());
 
-    //     skladRepository.save(updateSklad);
+    // skladRepository.save(updateSklad);
 
-    //     return ResponseEntity.ok(updateSklad);
+    // return ResponseEntity.ok(updateSklad);
     // }
 
     // //build delete employee REST API
     // @DeleteMapping("{id}")
     // public ResponseEntity<HttpStatus> deleteSklad(@PathVariable long id) {
-    //     Sklad sklad = skladRepository.findById(id)
-    //             .orElseThrow(() -> new ResourceNotFoundException("sklad not exist with id:" + id));
-    //     skladRepository.delete(sklad);
+    // Sklad sklad = skladRepository.findById(id)
+    // .orElseThrow(() -> new ResourceNotFoundException("sklad not exist with id:" +
+    // id));
+    // skladRepository.delete(sklad);
 
-    //     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    // return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     // }
-    
+
 }
